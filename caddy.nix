8395505b17f15487e2a222dcfbb0560f10bfb590
Erwin Boskma <erwin@datarift.nl>
2023-08-09 21:33:31 +0200

    Allow specifying a revision for plugins

--- caddy.nix
+++ caddy.nix
@@ -1,17 +1,26 @@
 { self, ... }: {
   perSystem = { self', pkgs, lib, ... }: {
-    packages.caddy-with-cloudflare = 
+    packages.caddy-with-cloudflare =
       self.lib.caddyWithPackages {
         inherit (pkgs) caddy buildGoModule;
-        plugins = [ "github.com/caddy-dns/cloudflare" ];
-        vendorSha256 = "iAGryqtiCKs8sj349zZsgHNtkTYTdVBtKtf+HbLsBio=";
+        plugins = [ "github.com/caddy-dns/cloudflare@a9d3ae2690a1d232bc9f8fc8b15bd4e0a6960eec" ];
+        vendorSha256 = "juhzEaAv3s8KAcyloSNotAddOqgMBqjOcTkbA15Gj/U=";
       };
   };
 
   flake = {
-    lib.caddyWithPackages = { caddy, buildGoModule, plugins, vendorSha256 }: let
-      pluginImports = builtins.concatStringsSep "\n" (map (plugin: "_ \"${plugin}\"") plugins);
-      
+    lib.caddyWithPackages = { caddy, buildGoModule, plugins, vendorSha256 }:
+      let
+        pluginImports = builtins.concatStringsSep "\n"
+          (map
+            (pluginWithHash:
+              let
+                plugin = builtins.elemAt (builtins.split "@" pluginWithHash) 0;
+              in
+              "_ \"${plugin}\"")
+            plugins);
+        pluginGoGetCmds = builtins.concatStringsSep "\n" (map (plugin: "go get ${plugin}") plugins);
+
         main = ''
           package main
 
@@ -30,12 +39,15 @@
       caddy.override {
         buildGoModule = args: buildGoModule ((builtins.removeAttrs args [ "vendorHash" "pname" ]) // {
           inherit vendorSha256;
-          
+
           pname = "caddy-with-plugins";
-          
+
           overrideModAttrs = _: {
             preBuild = "echo '${main}' > cmd/caddy/main.go";
-            postConfigure = "go mod tidy";
+            postConfigure = ''
+              ${pluginGoGetCmds}
+              go mod tidy
+            '';
             postInstall = ''
               mkdir -p "$out/.magic"
               cp go.sum go.mod "$out/.magic"
