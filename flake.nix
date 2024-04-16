8fd5b1c796d00b0fddd678c06fc76da0367245cc
Erwin Boskma <erwin@datarift.nl>
2024-04-16 16:56:12 +0200

    Use treefmt-nix to set up formatting

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,12 +15,32 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
-      imports = [ ./caddy.nix ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+
+        ./caddy.nix
+      ];
 
       perSystem =
-        { self', pkgs, ... }:
         {
-          formatter = pkgs.nixfmt-rfc-style;
+          self',
+          pkgs,
+          config,
+          ...
+        }:
+        {
+          formatter = config.treefmt.build.wrapper;
+
+          treefmt = {
+            projectRootFile = "flake.lock";
+
+            programs = {
+              nixfmt = {
+                enable = true;
+                package = pkgs.nixfmt-rfc-style;
+              };
+            };
+          };
 
           packages.default = self'.packages.caddy-with-cloudflare;
         };
